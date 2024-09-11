d5b311fd12813a24e69f361af7262e09e790f1f4
Sebastianx0808 <sebastain.jose@bcah.christuniversity.in>
2024-09-11 22:28:46 +0530

    Initial commit: Add all project files

---
+++ app/src/main/java/com/example/waterwise/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.example.waterwise
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_register)
+
+
+    }
+}
